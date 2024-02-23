82fe15a295279465e7aed50d9055e9b3a2b4a464
ariliem <156829193+ariliem@users.noreply.github.com>
2024-02-23 20:50:35 +0600

    Update 4.py

--- week6/regex/4.py
+++ week6/regex/4.py
@@ -1,28 +1,18 @@
-# import re
-
-# s = input()
-# pattern = r'[A-Z][a-z]+'
-
-# some = re.findall(pattern, s)
-
-
-# if some:
-#     print("Yes")
-
-# else:
-#     print("No")
-
-
 import re
 
 s = input()
-pattern = r"[A-Z][a-z]+"  # Regex for upper case followed by one or more lower case
-matches = re.findall(pattern, s)
-if matches:
+pattern = r'[A-Z][a-z]+'
+
+some = re.findall(pattern, s)
+
+
+if some:
     print("Yes")
 
 else:
     print("No")
+
+
 
 
 
