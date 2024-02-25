d8b31918d38e26d01dcf00f3b3683ae7da4cb583
ariliem <ar_rakhman@kbtu.kz>
2024-02-25 16:19:44 +0600

    upd

--- week6/regex/9.py
+++ week6/regex/9.py
@@ -1,19 +1,6 @@
-# import re
-# s = input()
-# x = re.sub(r"(\w)([A-Z])", r"\1 \2", s)
-# print(x)
-
-
 import re
+s = input()
+a = re.sub(r"(\w)([A-Z])", r"\1 \2", s)
+print(a)
 
-def test(pattern, testData, testNumber, expectedResult):
-    result = re.sub(pattern, r"\1 \2", testData)
-    print(result)
-    if result == expectedResult:
-        print(testNumber + " is passed!")
-    else: 
-        print(testNumber + " is not passed!")
 
-pattern = r'(\w)([A-Z])'
-test(pattern, "MySuperTest", "test1", "My Super Test")
-test(pattern, " MySuperTest IAmRobot", "test2", " My Super Test I Am Robot")
